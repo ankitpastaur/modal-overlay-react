7ac756da3e9ea339585937ac06e86518a77d2358
ankitpastor <pastorankit18@gmail.com>
2024-12-29 13:20:45 -0500

    Added basci astructure for modal overlay using react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="show-offer">
+        <button className="offer-btn">Show Offer</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
