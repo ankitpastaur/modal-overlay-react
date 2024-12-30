e3410cd5a803fc4a7808858b873fd0d837926ac7
ankitpastor <pastorankit18@gmail.com>
2024-12-30 13:28:19 -0500

    Added modal for overlay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import Modal from "./Modal";
 import "./App.css";
 
 function App() {
+  const [isShow, setIsShow] = useState(false);
+  const handleOpenModal = () => {
+    setIsShow(true);
+    console.log("handleOpenModal is working");
+  };
   return (
     <div className="App">
       <div className="show-offer">
-        <button className="offer-btn">Show Offer</button>
+        <button className="offer-btn" onClick={handleOpenModal}>
+          Show Offer
+        </button>
       </div>
+      {isShow && <Modal />}
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,13 @@
+const Modal = () => {
+  return (
+    <div className="modal">
+      <button className="close-btn">X</button>
+      <div className="modal-content">
+        Click the button below to accept the amazing offer
+      </div>
+      <button className="accept-btn">Accept offer</button>
+    </div>
+  );
+};
+
+export default Modal;
